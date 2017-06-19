977e689a91e6e769257260227e465e8495031c92
Vidlec <vidlec@gmail.com>
2017-06-19 15:56:56 +0200

    finished main algorithm

--- index.js
+++ index.js
@@ -1,72 +1,109 @@
+//In reality, Enigma could had only 10 connected pairs
+//Here its 13 pairs
 const plugboard = {
     input: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     output: "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
 }
 
+//Wheels definition
+//TODO: Write funtion which doesnt care about number of wheels
 const wheels = [{
-        position: 0,
+        position: 5,
         letters: "AJDKSIRUXBLHWTMCQGZNPYFVOE"
     },
     {
-                position: 0,
+        position: 3,
         letters: "VZBRGITYUPSDNHLXAWMJQOFECK"
     },
     {
-                position: 0,
+        position: 22,
         letters: "NZJHGRCXMYSWBOUFAIVLPEKQDT"
     }
 ]
 
-const reflector = 
+//Reflector
+const reflector = {
+    letters: "ESOVPZJAYQUIRHXLNFTGKDCMWB"
+}
 
-function rotate(){
+//We need to "reflect" the letters. So we simply reverse the array.
+//This way, you can use enigma for encryption as well as decryption
+//Big flaw is: letter can never become itself.
+function reflect(letter) {
+    return swap(letter, reflector.letters, reflector.letters.split("").reverse().join(""), 0);
+}
+
+//Initial settings for wheels. 
+function init(){
     for(let wheel of wheels){
-        if(wheel.position < 25){
-            wheel.position ++;
+        let letters = wheel.letters.split("");
+        let spliced = letters.splice(0,wheel.position);
+        letters = letters.concat(spliced);
+        wheel.letters = letters.join("");
+    }
+}
+
+//After each keypress we need to roatate the wheels
+//Key is to do something called double stepping. 
+//When one wheel reaches 25 rotate it once more and then rotate next wheel once
+function rotate() {
+    for (let wheel of wheels) {
+        if (wheel.position < 25) {
+            wheel.position++;
+            let letters = wheel.letters.split("")
+            let letter = letters.shift();
+            letters.push(letter);
+            wheel.letters = letters.join("");
             break;
-        }
-        else{
+        } else {
             wheel.position = 0;
         }
     }
 }
-for(let i = 0;i<58;i++){
-    rotate()
-}
-console.log(wheels);
 
+//Compare two arrays of letters
+function swap(letter, input, output) {
+    letter = input[output.indexOf(letter)];
+    return letter;
+}
+
+//Encode message
+//TODO: Write function for wheels passage (forwards and backwards)
 function encode(input) {
-    for (let letter of input) {
-        rotate();
-        //Plugboard
-        letter = swap(letter,plugboard.output,plugboard.input,0);
-        //Firstwheel
-        letter = swap(letter,wheels[0].letters,plugboard.output,wheels[0].position);
-        //All wheels
-        letter = swapWheels(letter);
+    return new Promise((resolve, reject) => {
+        let output = "";
+        init();
+        for (let letter of input) {
 
+            //Rotate wheels once BEFORE encoding
+            rotate();
+            //Plugboard
+            letter = swap(letter, plugboard.input, plugboard.output);
+            //Firstwheel
+            letter = swap(letter, plugboard.output, wheels[0].letters);
+            //Secondtwheel
+            letter = swap(letter, wheels[0].letters, wheels[1].letters);
+            //Thirdtwheel
+            letter = swap(letter, wheels[1].letters, wheels[2].letters);
+            //Reflector
+            letter = swap(letter, wheels[2].letters, reflector.letters);
+            letter = reflect(letter);
 
-        console.log(letter);
-    }
+            //All again backwards
+            //Thirdtwheel
+            letter = swap(letter, reflector.letters, wheels[2].letters);
+            //Secondtwheel
+            letter = swap(letter, wheels[2].letters, wheels[1].letters);
+            //Firstwheel
+            letter = swap(letter, wheels[1].letters, wheels[0].letters);
+            //Plugboard
+            letter = swap(letter, wheels[0].letters, plugboard.output);
+            //Output
+            letter = swap(letter, plugboard.output, plugboard.input);
+            output += letter;
+        }
+        resolve(output)
+    })
 }
 
-function swap(letter, output, input, position) {
-    letter = output[input.indexOf(letter) + position];
-    return letter;
-}
-function reflector(letter){
-    //return swap(letter,wheels[wheels.length - 1])
-}
-
-function swapWheels(letter){
-    for(let i = 0; i < wheels.length - 1; i++){
-        letter = swap(letter,wheels[i+1].letters,wheels[i].letters,wheels[i+1].position)
-    }
-    return letter;
-}
-
-encode("aha".toUpperCase());
-
-const parts = [
-    {type:"plugboard"}
-]
+encode("test".toUpperCase()).then(output => console.log(output));
