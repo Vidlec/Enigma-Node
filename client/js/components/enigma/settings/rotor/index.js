35e832e6429ee200599e9d966c52665874b0393e
Vidlec <vidlec@gmail.com>
2017-07-04 12:48:01 +0200

    Fixed arrow positions

--- client/js/components/enigma/settings/rotor/index.js
+++ client/js/components/enigma/settings/rotor/index.js
@@ -17,16 +17,18 @@ function collect(connect, monitor) {
 class Rotor extends Component{
     render(){
         const { connectDragSource, isDragging } = this.props;
+        const topMethod = (this.props.location === "selectedSlots") ? "add" : "dec";
+        const botMethod = (this.props.location === "selectedSlots") ? "dec" : "add";
         return(
             connectDragSource(            
                 <li style={{opacity: isDragging ? 0.3 : 1, cursor: "move"}} className="rotor">
                 <div className="label">{this.props.rotor.label}</div>
-                <i  className={(this.props.location === "selectedSlots") ? "fa-caret-down fa fa-3x" : "fa-caret-left fa fa-3x"}
-                    onClick={() => this.props.handleRotorSetting(this.props.index,"dec",this.props.location)}>
+                <i  className={(this.props.location === "selectedSlots") ? "fa-caret-up fa fa-3x" : "fa-caret-left fa fa-3x"}
+                    onClick={() => this.props.handleRotorSetting(this.props.index,topMethod,this.props.location)}>
                 </i>
                 <div id="position">{this.props.rotor.position}</div>
-                <i  className={(this.props.location === "selectedSlots") ? "fa-caret-up fa fa-3x" : "fa-caret-right fa fa-3x"}
-                    onClick={() => this.props.handleRotorSetting(this.props.index,"add",this.props.location)}>
+                <i  className={(this.props.location === "selectedSlots") ? "fa-caret-down fa fa-3x" : "fa-caret-right fa fa-3x"}
+                    onClick={() => this.props.handleRotorSetting(this.props.index,botMethod,this.props.location)}>
                 </i>
             </li>)
         );
